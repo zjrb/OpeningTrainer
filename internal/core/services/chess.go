c89df02dcf6b66eab63ca90ae59bd5f58e6f2251
Zachary Beach <zjrb2012359@gmail.com>
2025-01-30 07:53:19 -0700

    changed move ordering strategy

--- internal/core/services/chess.go
+++ internal/core/services/chess.go
@@ -10,11 +10,11 @@ type ChessService struct {
 	ChessEngine *ports.ChessEngine
 }
 
-func getIndex(white bool, move int) int {
+func getIndex(white bool, move int) (int, int) {
 	if white {
-		return ((move - 1) * 3) + 1
+		return ((move - 1) * 3) + 1, move
 	} else {
-		return ((move - 1) * 3) + 2
+		return ((move - 1) * 3) + 2, move + 1
 	}
 }
 
@@ -32,18 +32,22 @@ func (svc *ChessService) StartGame(opening []string, white bool) string {
 }
 
 func (svc *ChessService) PushMove(opening []string, white bool, move int) (string, error) {
-	idx := getIndex(white, move)
+	idx, _ := getIndex(white, move)
 	if idx > len(opening) {
-		return "", errors.New("Move Number greater than size")
+		return "", errors.New("invalid move")
 	}
 	return opening[idx], nil
 }
 
 func (svc *ChessService) ProcessMove(opening []string, white bool, moveNum int, move string) (string, error) {
-	idx := getIndex(white, moveNum)
+	idx, moveNum := getIndex(white, moveNum)
 	if opening[idx] == move {
-		return "Correct", nil
+		response, err := svc.PushMove(opening, !white, moveNum)
+		if err != nil {
+			return "Completed opening!", nil
+		}
+		return response, nil
 	} else {
-		return "Incorrect Move, Try Again", errors.New("The Wrong Move has been played")
+		return "Incorrect Move, Try Again", errors.New("incorrect move played")
 	}
 }
